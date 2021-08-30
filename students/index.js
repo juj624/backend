9f65011220af909b540854a6390c2ca670efc5b4
juj624 <julienpenot@outlook.com>
2021-08-30 17:31:27 +0200

    chapitre_02

--- students/index.js
+++ students/index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
-
+app.use(express.json());
 
 let students = ["Laura", "ramad", "emran", "anthony", "julien"];
 
 app.get('/students', (req, res) => {
-    res.send(students);
+    res.json(students);
 });
 
 app.post("/students", (req, res) => {
     const body = req.body
     students.push(body.name);
+    res.json(students);
 });
 
 
